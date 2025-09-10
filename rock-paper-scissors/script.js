03fb542f0c67e8deb7792462653ca7a9b85a5298
NathanKoerschner <nathankoerschner@gmail.com>
2025-09-10 16:22:38 -0400

    Get computer chioce

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,28 @@
 console.log("hello world");
+
+/*
+ * getComputerChoice Function
+ * set computerChoice = undefined
+ * get a random number between 1 and 3
+ * if it's 1, give rock
+ * if it's 2, give paper
+ * if it's 3, give scissors
+ * return computerChoice
+ */
+
+function getComputerChoice() {
+  let computerChoice;
+  let computerChoicePrecursor = Math.floor(Math.random() * 2) + 1;
+  switch (computerChoicePrecursor) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
+}
